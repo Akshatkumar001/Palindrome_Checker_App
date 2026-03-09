f3d97ad0803aa9dae640ad2e56b624e1a1724881
Akshatkumar001 <iglxakshat@gmail.com>
2026-03-09 10:25:35 +0530

    UC11: object oriented palindrome

---
+++ src/src/UseCase11PalindromeCheckerApp.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class UseCase11PalindromeCheckerApp {
+
+    public static boolean checkPalindrome(String input) {
+
+        int start = 0;
+        int end = input.length() - 1;
+        while (start < end) {
+
+            if (input.charAt(start) != input.charAt(end)) {
+                return false;
+            }
+
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String input = "racecar";
+
+        boolean result = checkPalindrome(input);
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + result);
+
+        sc.close();
+    }
+}
